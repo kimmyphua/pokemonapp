aeacf2c7df4c5ac16abfb1c7e4dca865536ffbe1
kimmyphua <kimberlyphuaweyhan@gmail.com>
2021-09-16 13:40:38 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import PokemonList from './Component/PokemonList';
+import PokemonTable from './Component/PokemonTable';
+import ThemedButton from './Component/ThemedButton';
+import Toolbar from './Component/Toolbar';
+import { PokemonProvider } from './Context/PokemonContext';
+import { ThemeProvider } from './Context/ThemeContext';
+
+
+function App() {
+  return (
+    <div className="App">
+       
+      {/* <ThemeProvider> */}
+        <PokemonProvider>
+          <PokemonList/>
+          {/* <PokemonTable/> */}
+        {/* <ThemedButton /> */}
+        </PokemonProvider> 
+      {/* </ThemeProvider> */}
+    </div>
+  );
+}
+
+export default App;
